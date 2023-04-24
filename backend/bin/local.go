0154219839dffc570b9305185a490c64749da4d3
wangdashuaihenshuai <wangjinhai68@gmail.com>
2023-04-24 16:28:09 +0800

    升级后端依赖。

--- backend/bin/local.go
+++ backend/bin/local.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"log"
+
+	"github.com/HuolalaTech/page-spy-api/config"
+	"github.com/HuolalaTech/page-spy-api/container"
 	"github.com/HuolalaTech/page-spy-api/serve"
 )
 
 func main() {
-	serve.Run(nil)
+	container := container.Container()
+	err := container.Provide(func() *config.StaticConfig {
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	serve.Run()
 }
