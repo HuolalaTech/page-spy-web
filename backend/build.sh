e3d0d821aeaac4be9e564256779d90c0cc507b48
wangdashuaihenshuai <wangjinhai68@gmail.com>
2023-07-07 10:28:34 +0800

    delete package node version dep

--- backend/build.sh
+++ backend/build.sh
@@ -23,9 +23,6 @@ GenerateMainPackageJson() {
     "registry": "https://registry.npmjs.org",
     "access": "public"
   },
-  "engines": {
-    "node": ">=17"
-  },
   "bin": {
     "${project_name}": "bin/${project_name}"
   },
@@ -120,9 +117,6 @@ PublishAndGeneratePackageJson() {
   },
   "license": "MIT",
   "preferUnplugged": true,
-  "engines": {
-    "node": ">=17"
-  },
   "os": [
     "${os}"
   ],
