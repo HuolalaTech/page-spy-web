b2520ff4c40d766b9a7334ec1c4857c08415f67a
blucas.wu <15555602203@163.com>
2023-08-08 15:23:41 +0800

    update list room api params

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,11 @@
 import { default as request } from './request';
 
-export const getSpyRoom = (group: string) => {
+export const getSpyRoom = (group: string = '') => {
   return request.get<I.SpyRoomList>(`/room/list`, {
-    params: {
-      group,
-    },
+    params: group?.trim()
+      ? {
+          group,
+        }
+      : {},
   });
 };
