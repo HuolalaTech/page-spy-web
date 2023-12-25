f86644413c1cdc432bb9c8018765a07ceaf4c495
notfound945 <732039303@qq.com>
2023-12-25 18:52:33 +0800

    feat: add icon for log level options

--- src/pages/Devtools/ConsolePanel/components/HeaderActions/index.tsx
+++ src/pages/Devtools/ConsolePanel/components/HeaderActions/index.tsx
@@ -1,8 +1,15 @@
 import { useSocketMessageStore } from '@/store/socket-message';
 import { ClearOutlined } from '@ant-design/icons';
 import { Row, Col, Tooltip, Button, Select, Space } from 'antd';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ProxyType } from '@huolala-tech/page-spy/dist/types/lib/console';
+import { ReactComponent as ErrorSvg } from '@/assets/image/error.svg';
+import { ReactComponent as InfoSvg } from '@/assets/image/info.svg';
+import { ReactComponent as WarnSvg } from '@/assets/image/warn.svg';
+import { ReactComponent as UserSvg } from '@/assets/image/user.svg';
+import { ReactComponent as DebugSvg } from '@/assets/image/debug.svg';
+import './index.less';
 
 export const HeaderActions = () => {
   const { t } = useTranslation();
@@ -10,15 +17,55 @@ export const HeaderActions = () => {
     (state) => [state.clearRecord, state.setConsoleMsgTypeFilter],
   );
 
-  const logLevelList = [
-    { label: 'User messages', value: 'log' },
-    { label: 'Errors', value: 'error' },
+  const logLevelList: Array<{
+    label: string | React.ReactNode;
+    value: ProxyType;
+  }> = [
     {
-      label: 'Warnings',
+      label: (
+        <div className="select-item">
+          <UserSvg style={{ height: 15, width: 15 }} />
+          <span className="select-item label-text">User messages</span>
+        </div>
+      ),
+      value: 'log',
+    },
+    {
+      label: (
+        <div className="select-item">
+          <ErrorSvg style={{ height: 15, width: 15 }} />
+          <span className="select-item label-text">Errors</span>
+        </div>
+      ),
+      value: 'error',
+    },
+    {
+      label: (
+        <div className="select-item">
+          <WarnSvg style={{ height: 15, width: 15 }} />
+          <span className="select-item label-text">Warnings</span>
+        </div>
+      ),
       value: 'warn',
     },
-    { label: 'Info', value: 'info' },
-    { label: 'Verbose', value: 'debug' },
+    {
+      label: (
+        <div className="select-item">
+          <InfoSvg style={{ height: 15, width: 15 }} />
+          <span className="select-item label-text">Info</span>
+        </div>
+      ),
+      value: 'info',
+    },
+    {
+      label: (
+        <div className="select-item">
+          <DebugSvg style={{ height: 15, width: 15 }} />
+          <span className="select-item label-text">Verbose</span>
+        </div>
+      ),
+      value: 'debug',
+    },
   ];
 
   const clear = useCallback(() => {
@@ -32,6 +79,7 @@ export const HeaderActions = () => {
             onChange={changeConsoleMsgFilter}
             maxTagCount="responsive"
             mode="multiple"
+            allowClear={true}
             options={logLevelList}
             placeholder="Log Level Filter"
             style={{ width: 200 }}
