3db9f581d2191fb6a233f1ac5b1e38bc246276fa
Blucas <15555602203@163.com>
2024-04-16 10:32:34 +0800

    update

--- src/pages/Devtools/ConsolePanel/index.tsx
+++ src/pages/Devtools/ConsolePanel/index.tsx
@@ -4,14 +4,23 @@ import { HeaderActions } from './components/HeaderActions';
 import { MainContent } from './components/MainContent';
 import { FooterInput } from './components/FooterInput';
 import { ErrorDetailDrawer } from './components/ErrorDetailDrawer';
+import { useClientInfoFromMsg } from '@/utils/brand';
+import { useMemo } from 'react';
 
 const ConsolePanel = () => {
+  const clientInfo = useClientInfoFromMsg();
+  const dynamicalExecutable = useMemo(() => {
+    const { os } = clientInfo || {};
+    // TODO 纯血鸿蒙出来后需要额外判断是「鸿蒙上的 APP」
+    return os?.type !== 'harmony';
+  }, [clientInfo]);
+
   return (
     <div className="console-panel">
       <HeaderActions />
       <div className="console-panel__content">
         <MainContent />
-        <FooterInput />
+        {dynamicalExecutable && <FooterInput />}
       </div>
       <ErrorDetailDrawer />
     </div>
