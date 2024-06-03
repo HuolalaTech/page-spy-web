9a6fb3395a127b739f64142cef55b1a62a6b90c9
Blucas <15555602203@163.com>
2024-06-03 15:36:25 +0800

    feat: limit showed connections in /room-list

--- src/pages/RoomList/index.tsx
+++ src/pages/RoomList/index.tsx
@@ -2,6 +2,7 @@ import { getSpyRoom } from '@/apis';
 import {
   AllBrowserTypes,
   AllMPTypes,
+  ClientRoomInfo,
   OS_CONFIG,
   getBrowserLogo,
   getBrowserName,
@@ -26,12 +27,15 @@ import { useTranslation } from 'react-i18next';
 import './index.less';
 import { ClearOutlined, SearchOutlined } from '@ant-design/icons';
 import { RoomCard } from './RoomCard';
+import { Statistics } from './Statistics';
 
 const { Title } = Typography;
 const { Option } = Select;
 const { Sider, Content } = Layout;
 
-const sortConnections = (data: I.SpyRoom[]) => {
+const MAXIMUM_CONNECTIONS = 30;
+
+const sortConnections = (data: ClientRoomInfo[]) => {
   const [valid, invalid] = (data || []).reduce(
     (acc, cur) => {
       const hasClient =
@@ -62,7 +66,7 @@ const sortConnections = (data: I.SpyRoom[]) => {
 };
 
 const filterConnections = (
-  data: I.SpyRoom[],
+  data: ClientRoomInfo[],
   condition: Record<'title' | 'address' | 'os' | 'browser', string>,
 ) => {
   const { title = '', address = '', os = '', browser = '' } = condition;
@@ -72,8 +76,7 @@ const filterConnections = (
       return String(tags.title).toLowerCase().includes(lowerCaseTitle);
     })
     .filter((i) => i.address.slice(0, 4).includes(address || ''))
-    .filter(({ name }) => {
-      const clientInfo = parseUserAgent(name);
+    .filter((clientInfo) => {
       return (
         (!os || clientInfo.os.type === os) &&
         (!browser || clientInfo.browser.type.includes(browser))
@@ -85,6 +88,9 @@ export const RoomList = () => {
   const [form] = Form.useForm();
   const { t } = useTranslation();
 
+  const showDashboard = localStorage.getItem('page-spy-dashboard') === '1';
+
+  const [showMaximumAlert, setMaximumAlert] = useState(false);
   const {
     data: connectionList = [],
     error,
@@ -170,10 +176,15 @@ export const RoomList = () => {
         />
       );
 
-    const list = sortConnections(filterConnections(connectionList, conditions));
+    const matchedConnections = filterConnections(connectionList, conditions);
+    setMaximumAlert(matchedConnections.length > MAXIMUM_CONNECTIONS);
+
+    const list = sortConnections(
+      matchedConnections.slice(0, MAXIMUM_CONNECTIONS),
+    );
 
     return (
-      <Row gutter={24}>
+      <Row gutter={24} style={{ padding: 24 }}>
         {list.map((room) => (
           <RoomCard key={room.address} room={room} />
         ))}
@@ -183,102 +194,111 @@ export const RoomList = () => {
 
   return (
     <Layout style={{ height: '100%' }} className="room-list">
-      <Sider width={350} theme="light" style={{ padding: 24 }}>
-        <Title level={3} style={{ marginBottom: 32 }}>
-          {t('common.connections')}
-        </Title>
-        <Form layout="vertical" form={form} onFinish={onFormFinish}>
-          <Form.Item label={t('common.device-id')} name="address">
-            <Input placeholder={t('common.device-id')!} allowClear />
-          </Form.Item>
-          <Form.Item label={t('common.project')} name="project">
-            <Input placeholder={t('common.project')!} allowClear />
-          </Form.Item>
-          <Form.Item label={t('common.title')} name="title">
-            <Input placeholder={t('common.title')!} allowClear />
-          </Form.Item>
-          <Form.Item label={t('common.os')} name="os">
-            <Select placeholder={t('connections.select-os')} allowClear>
-              {Object.entries(OS_CONFIG).map(([name, conf]) => {
-                return (
-                  <Option value={name} key={name}>
-                    <div className="flex-between">
-                      <span>{conf.label}</span>
-                      <img src={conf.logo} height="20" alt="" />
-                    </div>
-                  </Option>
-                );
-              })}
-            </Select>
-          </Form.Item>
-          <Form.Item label={t('common.browser')} name="browser">
-            <Select
-              listHeight={500}
-              placeholder={t('connections.select-browser')}
-              allowClear
-            >
-              {!!BrowserOptions.length && (
-                <Select.OptGroup label="Web" key="web">
-                  {BrowserOptions.map(({ name, logo, label }) => {
-                    return (
-                      <Option key={name} value={name}>
-                        <div className="flex-between">
-                          <span>{label}</span>
-                          <img src={logo} width="20" height="20" alt="" />
-                        </div>
-                      </Option>
-                    );
-                  })}
-                </Select.OptGroup>
-              )}
+      <Sider width={350} theme="light">
+        <div className="room-list-sider">
+          <Title level={3} style={{ marginBottom: 32 }}>
+            {t('common.connections')}
+          </Title>
+          <Form layout="vertical" form={form} onFinish={onFormFinish}>
+            <Form.Item label={t('common.device-id')} name="address">
+              <Input placeholder={t('common.device-id')!} allowClear />
+            </Form.Item>
+            <Form.Item label={t('common.project')} name="project">
+              <Input placeholder={t('common.project')!} allowClear />
+            </Form.Item>
+            <Form.Item label={t('common.title')} name="title">
+              <Input placeholder={t('common.title')!} allowClear />
+            </Form.Item>
+            <Form.Item label={t('common.os')} name="os">
+              <Select placeholder={t('connections.select-os')} allowClear>
+                {Object.entries(OS_CONFIG).map(([name, conf]) => {
+                  return (
+                    <Option value={name} key={name}>
+                      <div className="flex-between">
+                        <span>{conf.label}</span>
+                        <img src={conf.logo} height="20" alt="" />
+                      </div>
+                    </Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+            <Form.Item label={t('common.browser')} name="browser">
+              <Select
+                listHeight={500}
+                placeholder={t('connections.select-browser')}
+                allowClear
+              >
+                {!!BrowserOptions.length && (
+                  <Select.OptGroup label="Web" key="web">
+                    {BrowserOptions.map(({ name, logo, label }) => {
+                      return (
+                        <Option key={name} value={name}>
+                          <div className="flex-between">
+                            <span>{label}</span>
+                            <img src={logo} width="20" height="20" alt="" />
+                          </div>
+                        </Option>
+                      );
+                    })}
+                  </Select.OptGroup>
+                )}
 
-              {!!MPTypeOptions.length && (
-                <Select.OptGroup
-                  label={t('common.miniprogram')}
-                  key="miniprogram"
-                >
-                  {MPTypeOptions.map(({ name, logo, label }) => {
-                    return (
-                      <Option key={name} value={name}>
-                        <div className="flex-between">
-                          <span>{label}</span>
-                          <img src={logo} width="20" height="20" alt="" />
-                        </div>
-                      </Option>
-                    );
-                  })}
-                </Select.OptGroup>
-              )}
-            </Select>
-          </Form.Item>
-          <Row justify="end">
-            <Col>
-              <Form.Item>
-                <Space>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    icon={<SearchOutlined />}
+                {!!MPTypeOptions.length && (
+                  <Select.OptGroup
+                    label={t('common.miniprogram')}
+                    key="miniprogram"
                   >
-                    {t('common.search')}
-                  </Button>
-                  <Button
-                    type="default"
-                    icon={<ClearOutlined />}
-                    onClick={() => {
-                      form.resetFields();
-                      form.submit();
-                    }}
-                  >
-                    {t('common.reset')}
-                  </Button>
-                </Space>
-              </Form.Item>
-            </Col>
-          </Row>
-        </Form>
+                    {MPTypeOptions.map(({ name, logo, label }) => {
+                      return (
+                        <Option key={name} value={name}>
+                          <div className="flex-between">
+                            <span>{label}</span>
+                            <img src={logo} width="20" height="20" alt="" />
+                          </div>
+                        </Option>
+                      );
+                    })}
+                  </Select.OptGroup>
+                )}
+              </Select>
+            </Form.Item>
+            <Row justify="end">
+              <Col>
+                <Form.Item>
+                  <Space>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      icon={<SearchOutlined />}
+                    >
+                      {t('common.search')}
+                    </Button>
+                    <Button
+                      type="default"
+                      icon={<ClearOutlined />}
+                      onClick={() => {
+                        form.resetFields();
+                        form.submit();
+                      }}
+                    >
+                      {t('common.reset')}
+                    </Button>
+                  </Space>
+                </Form.Item>
+              </Col>
+            </Row>
+
+            {showMaximumAlert && (
+              <div className="maximum-alert">
+                {t('connections.maximum-alert')}
+              </div>
+            )}
+          </Form>
+          {showDashboard && <Statistics data={connectionList} />}
+        </div>
       </Sider>
-      <Content style={{ padding: 24 }}>{mainContent}</Content>
+      <Content>{mainContent}</Content>
     </Layout>
   );
 };
