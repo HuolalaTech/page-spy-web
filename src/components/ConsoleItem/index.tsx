ed73a8b076d811b5291c3cfc4bef016a117f664e
Blucas <15555602203@163.com>
2024-07-22 17:51:35 +0800

    feat: support serialized data in console panel

--- src/components/ConsoleItem/index.tsx
+++ src/components/ConsoleItem/index.tsx
@@ -15,12 +15,15 @@ import { getLogUrl } from '@/utils';
 import './index.less';
 import Timestamp from '../Timestamp';
 import { useMemo } from 'react';
+import ReactJsonView from '@huolala-tech/react-json-view';
+import { useTranslation } from 'react-i18next';
 
 interface Props {
   data: SpyConsole.DataItem;
 }
 
 export const ConsoleItem = ({ data }: Props) => {
+  const { t } = useTranslation();
   const content = useMemo(() => {
     if (isPlaceholderNode(data)) {
       return <PlaceholderNode data={data.logs} />;
@@ -34,9 +37,19 @@ export const ConsoleItem = ({ data }: Props) => {
       if (framesOfErrorConsole) {
         return <ErrorTraceNode data={framesOfErrorConsole} key={log.id} />;
       }
+      if (log.type === 'json') {
+        if (log.value === null) {
+          return (
+            <code key={log.id} className="non-serializable">
+              {t('console.non-serializable')}
+            </code>
+          );
+        }
+        return <ReactJsonView source={JSON.parse(log.value)} key={log.id} />;
+      }
       return <ConsoleNode data={log} key={log.id} />;
     });
-  }, [data]);
+  }, [data, t]);
   return (
     <div className={`console-item ${data.logType}`} key={data.id}>
       <div className="console-item__title">
@@ -47,7 +60,9 @@ export const ConsoleItem = ({ data }: Props) => {
           <Col style={{ flexShrink: 0 }}>
             <Timestamp time={data.time} />
           </Col>
-          <Col flex={1}>{content}</Col>
+          <Col flex={1} style={{ display: 'flex' }}>
+            {content}
+          </Col>
         </Row>
       </div>
       <div className="console-item__url" title={data.url}>
