0c61c7a479c6fd4b4e698054e9f16661f3e2c0ad
blucas.wu <15555602203@163.com>
2024-07-27 15:57:38 +0800

    update changelog about 1.9.1 and format links which are related to PageSpy repo

--- src/pages/Docs/components/DocContent/mdx-mapping/A.tsx
+++ src/pages/Docs/components/DocContent/mdx-mapping/A.tsx
@@ -1,17 +1,34 @@
 import { PropsWithChildren } from 'react';
 import { TransitionLink } from '@/components/Transition';
 import { useNavigate } from 'react-router-dom';
+import { isString } from 'lodash-es';
+
+const GITHUB_REPO_LINK =
+  /https:\/\/github\.com\/HuolalaTech\/(page-spy.*?)\/(pull|issues)\/(\d+)/;
 
 // 超链接可能使用环境变量，使用格式
 // - 正常：[xxx](https://xxx/yyy)
-// - 使用变量: [xxx]({VITE_XXX})
-// - 相对路径: [xxx](./xxx)
-const A = (props: PropsWithChildren<any>) => {
+// - 支持 gfm 自动识别链接：如果是 github 仓库相关的，如 https://github.com/HuolalaTech/page-spy-web/pull/239 会进一步处理成 <repo>#<id>
+// - 支持使用环境变量: [xxx]({VITE_XXX})
+// - 支持相对路径的文档过渡效果: [xxx](./xxx)
+const A = (props: PropsWithChildren<{ href: string; children: string }>) => {
   const navigate = useNavigate();
 
   const { children, href } = props;
-
   const decodeHref = decodeURI(href as string);
+
+  if (isString(children)) {
+    const githubMatchd = children.match(GITHUB_REPO_LINK);
+    if (githubMatchd !== null) {
+      const [link, repo, type, id] = githubMatchd;
+      return (
+        <a href={link} target="_blank">
+          {`${repo}#${id}`}
+        </a>
+      );
+    }
+  }
+
   const url = decodeHref.replace(/\{VITE_(.*?)\}/, (_, key) => {
     return import.meta.env[`VITE_${key}`];
   });
