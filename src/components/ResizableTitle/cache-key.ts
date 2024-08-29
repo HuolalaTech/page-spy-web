b261d023c4fab7b15260bc8a2eb872b8b052404b
Blucas <15555602203@163.com>
2024-08-29 18:02:03 +0800

    feat: storage table now resizable

---
+++ src/components/ResizableTitle/cache-key.ts
@@ -0,0 +1,5 @@
+export const ONLINE_NETWORK_CACHE = 'online:network-table-resize-config';
+export const OFFLINE_NETWORK_CACHE = 'offline:network-table-resize-config';
+
+export const ONLINE_STORAGE_CACHE = 'online:storage-table-resize-config';
+export const OFFLINE_STORAGE_CACHE = 'offline:storage-table-resize-config';
--- src/components/StorageTable/index.tsx
+++ src/components/StorageTable/index.tsx
@@ -1,36 +1,181 @@
 import { useCacheDetailStore } from '@/store/cache-detail';
 import { StorageType } from '@/store/platform-config';
 import { SpyStorage } from '@huolala-tech/page-spy-types';
-import { Table, Tooltip } from 'antd';
+import { Table, TableColumnsType, Tooltip } from 'antd';
 import { capitalize } from 'lodash-es';
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
+import { ResizableTitle } from '../ResizableTitle';
+import { ResizeCallbackData } from 'react-resizable';
 
-const { Column } = Table;
+const allCols = [
+  {
+    dataIndex: 'name',
+    title: 'Name',
+    ellipsis: true,
+    width: 200,
+  },
+  {
+    dataIndex: 'value',
+    title: 'Value',
+    ellipsis: true,
+    width: 300,
+  },
+  {
+    dataIndex: 'domain',
+    title: 'Domain',
+    ellipsis: true,
+    width: 200,
+  },
+  {
+    dataIndex: 'path',
+    title: 'Path',
+    ellipsis: true,
+    width: 100,
+  },
+  {
+    dataIndex: 'expires',
+    title: 'Expires',
+    ellipsis: true,
+    width: 240,
+    render: (value: string) => {
+      const time = value ? new Date(value).toISOString() : 'Session';
+      return (
+        <Tooltip placement="topLeft" title={time}>
+          {time}
+        </Tooltip>
+      );
+    },
+  },
+  {
+    dataIndex: 'secure',
+    title: 'Secure',
+    ellipsis: true,
+    width: 80,
+    render: (bool: boolean) => bool && '✅',
+  },
+  {
+    dataIndex: 'sameSite',
+    title: 'SameSite',
+    ellipsis: true,
+    width: 80,
+    render: (v: string) => capitalize(v),
+  },
+  {
+    dataIndex: 'partitioned',
+    title: 'Partitioned',
+    ellipsis: true,
+  },
+];
 
 interface Props {
   activeTab: StorageType;
   storageMsg: Record<StorageType, SpyStorage.GetTypeDataItem['data']>;
+  resizeCacheKey: string;
 }
 
-export const StorageTable = ({ activeTab, storageMsg }: Props) => {
+export const StorageTable = ({
+  activeTab,
+  storageMsg,
+  resizeCacheKey,
+}: Props) => {
   const data = useMemo(() => {
     return Object.values(storageMsg[activeTab]);
   }, [activeTab, storageMsg]);
   const hasDetail = useMemo(() => {
+    console.log({ data });
     const { name, value, ...rest } = data[0] || {};
     return Object.keys(rest).length > 0;
   }, [data]);
 
+  const unionCacheKey = useMemo(
+    () => `${resizeCacheKey}:${activeTab}`,
+    [resizeCacheKey, activeTab],
+  );
+  const cacheWidthRef = useRef<Record<string, { [title: string]: number }>>({});
+
+  const [columns, setColumns] =
+    useState<TableColumnsType<SpyStorage.Data>>(allCols);
+
+  // Init width from cache
+  useMemo(() => {
+    const cache = localStorage.getItem(unionCacheKey);
+    const value = cache && JSON.parse(cache);
+    if (value) {
+      cacheWidthRef.current[unionCacheKey] = value;
+      const widthInitedColumns = [...allCols].map((i) => {
+        return {
+          ...i,
+          width: value[i.title] || i.width,
+        };
+      });
+      setColumns(widthInitedColumns);
+    } else {
+      cacheWidthRef.current[unionCacheKey] = {};
+    }
+  }, [unionCacheKey]);
+
+  // Dragging
+  const mergedColumns = useMemo(() => {
+    let renderCols = [...columns];
+    if (!hasDetail && renderCols.length !== 2) {
+      renderCols = renderCols.slice(0, 2);
+    }
+    const cacheWidth = cacheWidthRef.current[unionCacheKey];
+    if (cacheWidth) {
+      renderCols.forEach((i) => {
+        const width = cacheWidth[i.title as string];
+        if (width) {
+          i.width = width;
+        }
+      });
+    }
+
+    return renderCols.map((c, index) => ({
+      ...c,
+      onHeaderCell: (column: TableColumnsType<SpyStorage.Data>[number]) => ({
+        width: c.width,
+        onResize: ((
+          _: React.SyntheticEvent<Element>,
+          data: ResizeCallbackData,
+        ) => {
+          const { size } = data;
+
+          const newCols = [...renderCols];
+          newCols[index] = {
+            ...newCols[index],
+            width: size.width,
+          };
+          setColumns(newCols);
+
+          cacheWidthRef.current[unionCacheKey][column.title as string] =
+            size.width;
+        }) as React.ReactEventHandler<any>,
+        onResizeStop: (_: React.SyntheticEvent, data: ResizeCallbackData) => {
+          const refValue = cacheWidthRef.current[unionCacheKey];
+
+          localStorage.setItem(unionCacheKey, JSON.stringify(refValue));
+        },
+      }),
+    }));
+  }, [columns, hasDetail, unionCacheKey]);
+
   const setDetailInfo = useCacheDetailStore((state) => state.setCurrentDetail);
+
+  console.log({ data, mergedColumns });
 
   return (
     <Table
+      className="storage-table"
       rowKey="name"
       bordered={false}
       dataSource={data}
       pagination={false}
       tableLayout="fixed"
       size="small"
+      columns={mergedColumns}
+      style={{
+        overflowX: 'hidden',
+      }}
       onRow={(record) => {
         return {
           onClick() {
@@ -38,42 +183,11 @@ export const StorageTable = ({ activeTab, storageMsg }: Props) => {
           },
         };
       }}
-    >
-      <Column title="Name" dataIndex="name" key="name" ellipsis />
-      <Column title="Value" dataIndex="value" ellipsis />
-      {hasDetail && (
-        <>
-          <Column title="Domain" dataIndex="domain" ellipsis />
-          <Column title="Path" width={120} dataIndex="path" ellipsis />
-          <Column
-            title="Expires"
-            dataIndex="expires"
-            ellipsis
-            render={(value) => {
-              const time = value ? new Date(value).toISOString() : 'Session';
-              return (
-                <Tooltip placement="topLeft" title={time}>
-                  {time}
-                </Tooltip>
-              );
-            }}
-          />
-          <Column
-            align="center"
-            title="Secure"
-            dataIndex="secure"
-            width={80}
-            render={(bool) => bool && '✅'}
-          />
-          <Column
-            title="SameSite"
-            dataIndex="sameSite"
-            width={80}
-            render={(v) => capitalize(v)}
-          />
-          <Column title="Partitioned" width={120} dataIndex="partitioned" />
-        </>
-      )}
-    </Table>
+      components={{
+        header: {
+          cell: ResizableTitle,
+        },
+      }}
+    />
   );
 };
