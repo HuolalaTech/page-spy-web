cf880ee6e9253f9b658bdca38219a43b676a81fe
Lgowen <434666361@qq.com>
2024-09-12 00:12:47 +0800

    feat: add filter options for network requests

--- src/pages/Devtools/NetworkPanel/index.tsx
+++ src/pages/Devtools/NetworkPanel/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-case-declarations */
-import { Button, Col, Row, Tooltip } from 'antd';
-import { memo, useEffect, useRef } from 'react';
+import { Row, Col, Tooltip, Button, Input, Select, Space } from 'antd';
+import { memo, useEffect, useRef, useMemo, useState, useCallback } from 'react';
 import { ClearOutlined } from '@ant-design/icons';
 import './index.less';
 import { useTranslation } from 'react-i18next';
@@ -8,40 +8,101 @@ import { useSocketMessageStore } from '@/store/socket-message';
 import { NetworkTable } from '@/components/NetworkTable';
 import { useForceThrottleRender } from '@/utils/useForceRender';
 import { ONLINE_NETWORK_CACHE } from '@/components/ResizableTitle/cache-key';
+import { debounce } from 'lodash-es';
 
 const NetworkPanel = memo(() => {
   const { t: ct } = useTranslation('translation', { keyPrefix: 'common' });
 
   const storeRef = useRef(useSocketMessageStore.getState());
-  const networkMsg = useRef(storeRef.current.networkMsg);
+  const [networkMsg, setNetworkMsg] = useState(storeRef.current.networkMsg);
   const storageMsg = useRef(storeRef.current.storageMsg);
   const clearRecord = useRef(storeRef.current.clearRecord);
   const { throttleRender } = useForceThrottleRender();
+  const [selectedRequestNames, setSelectedRequestNames] = useState<string[]>(
+    [],
+  );
+  const [networkKeyWord, setNetworkKeyWord] = useState('');
 
   useEffect(
     () =>
       useSocketMessageStore.subscribe((state) => {
-        networkMsg.current = state.networkMsg;
+        setNetworkMsg(state.networkMsg);
         storageMsg.current = state.storageMsg;
         throttleRender();
       }),
     [throttleRender],
   );
 
+  const RequestNameList = useMemo(() => {
+    return networkMsg.map((option) => ({
+      label: (
+        <div className="select-item">
+          <span className="select-item label-text">{option.pathname}</span>
+        </div>
+      ),
+      value: option.pathname,
+    }));
+  }, [networkMsg]);
+
+  const changeRequestNameFilter = (selectedNames: string[]) => {
+    setSelectedRequestNames(selectedNames);
+  };
+
+  const filteredNetworkMsg = useMemo(() => {
+    if (selectedRequestNames.length === 0 && networkKeyWord.length === 0) {
+      return networkMsg;
+    }
+
+    return networkMsg.filter((msg) => {
+      const matchesName =
+        selectedRequestNames.length === 0 ||
+        selectedRequestNames.includes(msg.pathname);
+      const matchesKeyword =
+        networkKeyWord.length === 0 || msg.pathname.includes(networkKeyWord);
+
+      return matchesName && matchesKeyword;
+    });
+  }, [networkKeyWord, networkMsg, selectedRequestNames]);
+
+  const debounceKeywordFilter = useCallback(
+    debounce((e) => {
+      console.log(e.target.value, 'e.target.value)');
+      setNetworkKeyWord(e.target.value);
+    }, 300),
+    [],
+  );
+
   return (
     <div className="network-panel">
       <Row justify="end">
         <Col>
-          <Tooltip title={ct('clear')}>
-            <Button onClick={() => clearRecord.current!('network')}>
-              <ClearOutlined />
-            </Button>
-          </Tooltip>
+          <Space>
+            <Select
+              onChange={changeRequestNameFilter}
+              maxTagCount="responsive"
+              mode="multiple"
+              allowClear={true}
+              options={RequestNameList}
+              placeholder="Request Name Filter"
+              style={{ width: 200 }}
+            />
+            <Input
+              onChange={debounceKeywordFilter}
+              placeholder="Keyword Filter"
+              allowClear={true}
+              style={{ width: 200 }}
+            />
+            <Tooltip title={ct('clear')}>
+              <Button onClick={() => clearRecord.current!('network')}>
+                <ClearOutlined />
+              </Button>
+            </Tooltip>
+          </Space>
         </Col>
       </Row>
       <div className="network-panel__content">
         <NetworkTable
-          data={networkMsg.current}
+          data={filteredNetworkMsg}
           cookie={storageMsg.current.cookie}
           resizeCacheKey={ONLINE_NETWORK_CACHE}
         />
