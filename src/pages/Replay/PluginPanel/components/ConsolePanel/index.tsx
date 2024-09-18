0270c82099ad16724a5a463d32ca24453df0f6e0
Blucas <15555602203@163.com>
2024-09-18 17:59:38 +0800

    disable spread object on atom node in replay

--- src/pages/Replay/PluginPanel/components/ConsolePanel/index.tsx
+++ src/pages/Replay/PluginPanel/components/ConsolePanel/index.tsx
@@ -3,15 +3,18 @@ import { useReplayStore } from '@/store/replay';
 import './index.less';
 import { memo } from 'react';
 import { useShallow } from 'zustand/react/shallow';
+import { ConfigProvider } from '@/components/ConfigProvider';
 
 export const ConsolePanel = memo(() => {
   const consoleMsg = useReplayStore(useShallow((state) => state.consoleMsg));
 
   return (
     <div className="console-panel">
-      {consoleMsg.map((data) => (
-        <ConsoleItem data={data} key={data.id} />
-      ))}
+      <ConfigProvider offline>
+        {consoleMsg.map((data) => (
+          <ConsoleItem data={data} key={data.id} />
+        ))}
+      </ConfigProvider>
     </div>
   );
 });
