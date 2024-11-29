419712cca9beb1d1ff93d0352172f37a3e34267b
Blucas <15555602203@163.com>
2024-11-29 15:46:44 +0800

    Add the shell to check changelog

---
+++ scripts/check-changelog.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(jq -r '.version' package.json)
+
+CHANGELOG_FILES=(
+  "src/pages/Docs/md/changelog.zh.mdx"
+  "src/pages/Docs/md/changelog.en.mdx"
+)
+
+for FILE in "${CHANGELOG_FILES[@]}"; do
+  if ! grep -q "$PACKAGE_VERSION" "$FILE"; then
+    echo "❌ Cannot find the description about $PACKAGE_VERSION in the $FILE"
+    echo "Rollback the version..."
+
+    git reset --hard HEAD^
+    git tag -d "$PACKAGE_VERSION"
+    PREV_VERSION=$(git describe --tags --abbrev=0)
+
+    echo "Version rollback to $PREV_VERSION"
+    exit 1
+  fi
+done
