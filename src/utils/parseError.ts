f09275068574ec604b8ff27aac37de7a0b1c8324
Blucas <15555602203@163.com>
2025-01-03 10:08:20 +0800

    Optimize error locate
    format error message and support locate source code which is not in js

--- src/utils/parseError.ts
+++ src/utils/parseError.ts
@@ -5,7 +5,7 @@ import type { Lang } from 'shiki';
 
 const getI18nText = (k: string) => getTranslation(`console.error-trace.${k}`);
 
-const fileCache = new Map<string, string>();
+const fileCache = new Map<string, { type: string; content: string }>();
 const readFile = async (url: string) => {
   try {
     if (!url.trim()) return null;
@@ -17,7 +17,11 @@ const readFile = async (url: string) => {
         return null;
       }
       const content = await res.text();
-      fileCache.set(url, content);
+      const type = res.headers.get('content-type') ?? '';
+      fileCache.set(url, {
+        type,
+        content,
+      });
     }
     return fileCache.get(url)!;
   } catch (e) {
@@ -25,102 +29,47 @@ const readFile = async (url: string) => {
   }
 };
 
-export interface Fragments {
-  line: number | null;
-  column: number | null;
-  start: number | null;
-  end: number | null;
-  sourceFile: string | null;
-  sourceHTML: string | null;
-  useTabs: boolean;
-}
-
-const locateSourceCode = (data: {
+const locateJsSource = (data: {
   sourcemap: string;
   lineNumber: number;
   columnNumber: number;
-}): Promise<Fragments> => {
+}): Promise<{
+  line: number;
+  column: number;
+  source: string;
+  content: string;
+}> => {
   const { sourcemap, lineNumber, columnNumber } = data;
-  return new Promise((resolve) => {
-    window.sourceMap.SourceMapConsumer.with(
-      sourcemap,
-      null,
-      async (consumer) => {
-        const { line, column, source } = consumer.originalPositionFor({
-          line: lineNumber,
-          column: columnNumber,
-        });
-        if (source === null || line === null || column === null) {
-          resolve({
-            line,
-            column,
-            start: null,
-            end: null,
-            sourceFile: source,
-            sourceHTML: null,
-            useTabs: false,
-          });
-          return;
-        }
-        const start = Math.max(line - 5, 0);
-        const end = line + 5;
-        const list =
-          consumer.sourceContentFor(source)?.split('\n').slice(start, end) ||
-          [];
+  return new Promise((resolve, reject) => {
+    window.sourceMap.SourceMapConsumer.with(sourcemap, null, (consumer) => {
+      const { line, column, source, name } = consumer.originalPositionFor({
+        line: lineNumber,
+        column: columnNumber,
+      });
+      if (!line || !column || !source) {
+        return reject(new Error(getI18nText('failed-title')));
+      }
 
-        const useTabs = list.some((i) => i.startsWith('\t'));
-        const sourceContent = list.join('\n');
-        const lang = getFileExtension(source) || 'js';
-        const highlighter = await sh.get({
-          lang: lang as Lang,
-        });
-        const tokens = highlighter.codeToThemedTokens(
-          sourceContent,
-          lang,
-          'github-dark',
-        );
-        let index = 0;
-        const sourceHTML = window.shiki.renderToHtml(tokens, {
-          bg: highlighter.getBackgroundColor('github-dark'),
-          elements: {
-            line({ className, children }) {
-              if (index++ === 4) {
-                return `<span class="${className} error-line" style="--position: ${column}">${children}</span>`;
-              }
-              return `<span class="${className}">${children}</span>`;
-            },
-          },
-        });
-        resolve({
-          line,
-          column,
-          start: start + 1,
-          end: end + 1,
-          sourceFile: source,
-          sourceHTML,
-          useTabs,
-        });
-      },
-    );
+      resolve({
+        line,
+        column,
+        source,
+        content: consumer.sourceContentFor(source)!,
+      });
+    });
   });
 };
 
 const loadSourceMap = async (filename: string) => {
   let result: string | null = null;
-
-  const originFileContent = await readFile(filename);
-  if (!originFileContent) {
-    throw new Error(getI18nText('fetch-minify-fail')!);
-  }
+  const { content } = (await readFile(filename))!;
 
   // inline sourcemap OR standalone sourcemap
-  const inlineContent = originFileContent.match(
-    /(?<=\/\/#\s+sourceMappingURL=).*/,
-  );
+  const inlineContent = content.match(/(?<=\/\/#\s+sourceMappingURL=).*/);
   if (!inlineContent) {
     // hidden sourcemap
     const fakeSourcemapFilename = filename + '.map';
-    result = await readFile(fakeSourcemapFilename);
+    result = (await readFile(fakeSourcemapFilename))?.content || null;
   } else {
     // inline sourcemap
     let targetUrl = inlineContent[0];
@@ -128,32 +77,92 @@ const loadSourceMap = async (filename: string) => {
       // standalone sourcemap
       targetUrl = new URL(targetUrl, filename).toString();
     }
-    result = await readFile(targetUrl);
+    result = (await readFile(targetUrl))?.content || null;
   }
 
   return result;
 };
 
+const highlightCode = async (data: {
+  type: string;
+  line: number;
+  column: number;
+  source: string;
+  content: string;
+}) => {
+  const { type, source, line, column, content } = data;
+  const start = Math.max(line - 5, 0);
+  const end = line + 5;
+  const list = content.split('\n').slice(start, end);
+
+  const useTabs = list.some((i) => i.startsWith('\t'));
+  const slicedContent = list.join('\n');
+  const lang = type.split('/')[1] || 'js';
+  const highlighter = await sh.get({
+    lang: lang as Lang,
+  });
+  const tokens = highlighter.codeToThemedTokens(
+    slicedContent,
+    lang,
+    'github-dark',
+  );
+  let index = 0;
+  const highlightedHTML = window.shiki.renderToHtml(tokens, {
+    bg: highlighter.getBackgroundColor('github-dark'),
+    elements: {
+      line({ className, children }) {
+        if (index++ === 4) {
+          return `<span class="${className} error-line" style="--position: ${column}">${children}</span>`;
+        }
+        return `<span class="${className}">${children}</span>`;
+      },
+    },
+  });
+
+  return {
+    line,
+    column,
+    start: start + 1,
+    end: end + 1,
+    source,
+    highlightedHTML,
+    useTabs,
+  };
+};
+
 /**
  * @description 从报错堆栈的文件、行、列反查源码，返回源码片段
  */
-export const getOriginFragments = async (data: {
-  fileName: string;
-  lineNumber: number;
-  columnNumber: number;
-}) => {
+export const getOriginFragments = async (data: Required<StackFrame>) => {
   const { fileName, lineNumber, columnNumber } = data;
+  const originFile = await readFile(fileName);
+  if (!originFile) {
+    throw new Error(getI18nText('fetch-minify-fail')!);
+  }
+  const { type, content } = originFile;
+  if (!type.includes('javascript')) {
+    return highlightCode({
+      type,
+      source: fileName,
+      content,
+      line: lineNumber,
+      column: columnNumber,
+    });
+  }
+
   const sourcemap = await loadSourceMap(fileName);
 
   if (!sourcemap) {
     throw new Error(getI18nText('fetch-sourcemap-fail'));
   }
 
-  const fragments = await locateSourceCode({
+  const fragments = await locateJsSource({
     sourcemap,
     lineNumber,
     columnNumber,
   });
-
-  return fragments;
+  return highlightCode({
+    type,
+    ...fragments,
+  });
 };
