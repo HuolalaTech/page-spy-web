696d8f37ab33b87ee207e24e1fb194ea22f3362d
Blucas <15555602203@163.com>
2025-01-15 00:15:37 +0800

    4/4 refacted done

--- src/pages/ReplayLab/components/ImportPackage/index.tsx
+++ src/pages/ReplayLab/components/ImportPackage/index.tsx
@@ -9,11 +9,8 @@ import Icon from '@ant-design/icons';
 
 const INIT_CODE = `const $feedback = new WholeBundle({
   title?: string; // 悬浮球和弹窗上显示的标题
-  
   logo?: string; // Logo 的图片路径
-  
   primaryColor?: string; // 定制主题色
-  
   autoRender?: boolean; // 默认渲染悬浮球，设置为 false 后可调用 $feedback.open() 打开弹窗
 })`;
 const methods = [
@@ -30,7 +27,10 @@ const methods = [
               </Space>
             ),
             lang: 'javascript',
-            code: `<script src="https://cdn.jsdelivr.net/npm/@huolala-tech/page-spy-plugin-whole-bundle/dist/iife/index.min.js" crossorigin="anonymous"></script>
+            code: `<script
+  src="https://cdn.jsdelivr.net/npm/@huolala-tech/page-spy-plugin-whole-bundle/dist/iife/index.min.js"
+  crossorigin="anonymous">
+</script>
 
 ${INIT_CODE}`,
           },
@@ -51,7 +51,7 @@ ${INIT_CODE}`,
     ),
   },
   {
-    title: 'ESM',
+    title: 'NPM',
     code: (
       <Flex vertical gap={12}>
         <h5>首先安装依赖：</h5>
@@ -60,7 +60,13 @@ ${INIT_CODE}`,
           code="yarn add @huolala-tech/page-spy-plugin-whole-bundle"
         />
         <h5>接着实例化：</h5>
-        <CodeBlock lang="javascript" code={INIT_CODE} />
+        <CodeBlock
+          lang="javascript"
+          code={`import WholeBundle from '@huolala-tech/page-spy-plugin-whole-bundle';
+import '@huolala-tech/page-spy-plugin-whole-bundle/dist/index.css';
+
+${INIT_CODE}`}
+        />
       </Flex>
     ),
   },
@@ -70,8 +76,15 @@ export const ImportPackage = () => {
   const [active, setActive] = useState<string>(methods[0].title);
   const activeMethod = methods.find((i) => i.title === active);
   return (
-    <Flex vertical align="center" gap={24} className="import-package">
-      <h1>按你喜欢的方式引入</h1>
+    <Flex
+      vertical
+      justify="center"
+      align="center"
+      gap={24}
+      className="import-package"
+    >
+      <h1 style={{ textAlign: 'center' }}>随心选择引入方式</h1>
+      <h5>与框架无关，你可以任选一种方式在项目中引入</h5>
       <Flex gap={8}>
         {methods.map(({ title }) => (
           <strong
@@ -87,7 +100,7 @@ export const ImportPackage = () => {
           </strong>
         ))}
       </Flex>
-      <div style={{ width: '70vw' }}>{activeMethod?.code}</div>
+      <div className="import-code">{activeMethod?.code}</div>
     </Flex>
   );
 };
--- src/pages/ReplayLab/components/Welcome.tsx
+++ src/pages/ReplayLab/components/Welcome.tsx
@@ -7,12 +7,22 @@ export const Welcome = () => {
   const next = useStepStore((state) => state.next);
 
   return (
-    <Flex vertical align="center" gap={40}>
-      <h1>欢迎来到回放实验室</h1>
-      <h5 style={{ textAlign: 'center', letterSpacing: 0.5 }}>
-        几行代码让你的系统拥有强大的「问题反馈」能力；
+    <Flex
+      vertical
+      justify="center"
+      align="center"
+      gap={40}
+      style={{ height: '100%' }}
+    >
+      <h1 style={{ textAlign: 'center' }}>
+        欢迎来到
         <br />
-        所有数据都在本地，不会经过服务器传输，无需担心隐私泄露
+        回放实验室
+      </h1>
+      <h5 style={{ textAlign: 'center', letterSpacing: 0.5 }}>
+        几行代码，让系统拥有强大的「问题反馈」能力
+        <br />
+        数据都在本地，不经过网络传输，无需担心隐私泄露
       </h5>
       <Button size="large" onClick={next}>
         <b>立刻体验</b>
