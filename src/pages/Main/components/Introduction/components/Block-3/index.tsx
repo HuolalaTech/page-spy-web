a4970e51e8c60dad8b607024313244ee89afc4b7
Blucas <15555602203@163.com>
2025-02-27 09:51:06 +0800

    feat: add community comments in home page

--- src/pages/Main/components/Introduction/components/Block-3/index.tsx
+++ src/pages/Main/components/Introduction/components/Block-3/index.tsx
@@ -1,22 +1,41 @@
-import { Button, Typography } from 'antd';
-import logoImg from '@/assets/image/logo.svg';
 import './index.less';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Trans, useTranslation } from 'react-i18next';
+import data from './comments.json';
+import { Avatar, Flex, theme } from 'antd';
+
+const { useToken } = theme;
 
 export const IntroBlock3 = () => {
   const { t } = useTranslation();
 
   return (
     <div className="intro-block block-3">
-      <div className="welcome-use">
-        <img src={logoImg} width="70" alt="LOGO" />
-        <Typography.Title level={2}>{t('intro.welcome')}</Typography.Title>
-        <Link to="/docs">
-          <Button type="primary" shape="round" size="large">
-            {t('common.doc')}
-          </Button>
-        </Link>
+      <div className="community-comments" style={{ textAlign: 'center' }}>
+        <Trans i18nKey="intro.community-comments">
+          <h1>受社区广大用户青睐</h1>
+          <h3>
+            不要只看我们说
+            <br />
+            来听听 PageSpy 社区中真实用户的声音
+          </h3>
+        </Trans>
+        <div className="comments-container">
+          {data.map((i) => (
+            <Flex key={i.html_url} vertical gap={24}>
+              <Flex gap={12} align="center">
+                <Avatar
+                  size="large"
+                  src={i.user.avatar_url}
+                  style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
+                >
+                  {i.user.login.slice(0, 1)}
+                </Avatar>
+                <b className="username">{i.user.login}</b>
+              </Flex>
+              <p className="comments-item">{i.body}</p>
+            </Flex>
+          ))}
+        </div>
       </div>
     </div>
   );
