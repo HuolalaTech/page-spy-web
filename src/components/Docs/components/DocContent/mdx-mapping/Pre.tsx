3760036ab4e8875e5c98d759c033f24af6405b20
Blucas <15555602203@163.com>
2025-10-24 10:41:16 +0800

    Add `enableSSL` option in the docs

--- src/components/Docs/components/DocContent/mdx-mapping/Pre.tsx
+++ src/components/Docs/components/DocContent/mdx-mapping/Pre.tsx
@@ -1,7 +1,6 @@
 import { PropsWithChildren, ReactElement } from 'react';
 import { CodeBlock } from '@/components/CodeBlock';
 import { useTranslation } from 'react-i18next';
-import { deployPath, deployUrl } from '@/utils/constants';
 
 const Pre = (props: PropsWithChildren<unknown>) => {
   const { t } = useTranslation();
@@ -9,22 +8,7 @@ const Pre = (props: PropsWithChildren<unknown>) => {
 
   const language = className?.match(/language-(.*)/)?.[1] || 'html';
 
-  // 代码块里可能会需要 i18n / 不同构建模式的输出不同 / 访问环境变量
-  // - 使用i18n：{t('xxx')}
-  // - 访问环境变量: {VITE_XXXX}
-  // - 注入特殊变量:
-  //   - {deployUrl}
-  //   - {deployPath}
-  const code = (children as string)
-    .replace(/\{t\(['"](.*?)['"]\)\}/g, (_, key) => {
-      return t(key);
-    })
-    .replace(/\{VITE_(.*?)\}/g, (_, key) => {
-      return import.meta.env[`VITE_${key}`];
-    })
-    .replace(/\{deployUrl\}/g, deployUrl)
-    .replace(/\{deployPath\}/g, deployPath);
-  return <CodeBlock code={code} lang={language} />;
+  return <CodeBlock code={children} lang={language} />;
 };
 
 export default Pre;
