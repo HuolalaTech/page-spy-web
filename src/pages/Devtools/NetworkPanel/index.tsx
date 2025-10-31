cea6946916606c3730d7b358e332416d61b0f613
Blucas <15555602203@163.com>
2025-10-31 11:01:01 +0800

    remove unused imports

--- src/pages/Devtools/NetworkPanel/index.tsx
+++ src/pages/Devtools/NetworkPanel/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-case-declarations */
 import { Row, Col, Tooltip, Button, Input, Space } from 'antd';
-import { memo, useRef, useState } from 'react';
+import { memo, useRef } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 import { ClearOutlined } from '@ant-design/icons';
 import './index.less';
@@ -8,8 +8,7 @@ import { useTranslation } from 'react-i18next';
 import { useSocketMessageStore } from '@/store/socket-message';
 import { NetworkTable } from '@/components/NetworkTable';
 import { ONLINE_NETWORK_CACHE } from '@/components/ResizableTitle/cache-key';
-import { debounce } from 'lodash-es';
-import { TypeFilter, NetworkType } from '@/components/NetworkTable/TypeFilter';
+import { TypeFilter } from '@/components/NetworkTable/TypeFilter';
 
 const NetworkPanel = memo(() => {
   const { t: ct } = useTranslation('translation', { keyPrefix: 'common' });
